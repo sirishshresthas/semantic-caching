75d78ee0e5b2f57f771431a43dd427901193237a
Sirish Shrestha <shresthas@ccl.org>
2024-02-26 20:45:03 -0500

    clean up

--- src/core/utilities/settings.py
+++ src/core/utilities/settings.py
@@ -2,19 +2,13 @@ import os
 
 from src.core.utilities import EXPORT_CONFIG
 
-# Define a default environment explicitly
-DEFAULT_ENV = "dev"
+ENV = os.getenv("PROJECT_ENVIRONMENT", "dev")
 
-ENV = os.getenv("PROJECT_ENVIRONMENT", DEFAULT_ENV)
-print("Env: ", ENV)
-
-# Get the configuration callable based on the environment
+# configuration callable based on the environment
 config_callable = EXPORT_CONFIG.get(ENV)
-print("Config: ", config_callable)
-# Ensure the configuration exists and is callable
+
 if config_callable and callable(config_callable):
     settings = config_callable()
 else:
-    # Handle missing or incorrect configuration (raise an error or use a default)
     raise ValueError(
         f"Missing or invalid configuration for environment: {ENV}")
